960b90cf4cfc30b3e50b79949a46f01fa31203f6
AnnaShev <anreich0@gmail.com>
2024-01-12 17:26:46 +0300

    AppOrderTest.java was modified

--- src/test/java/ru/netology/qahwweb/appordertests/AppOrderTest.java
+++ src/test/java/ru/netology/qahwweb/appordertests/AppOrderTest.java
@@ -1,4 +1,16 @@
 package ru.netology.qahwweb.appordertests;
 
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class AppOrderTest {
+    @Test
+    void shouldCreateRequestPositivePath() {
+        open("http://localhost:9999");
+    }
 }
