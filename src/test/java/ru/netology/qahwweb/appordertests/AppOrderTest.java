108ac570a3fa1ecde71881ebf4cd177b5960e411
AnnaShev <anreich0@gmail.com>
2024-01-15 12:29:18 +0300

    AppOrderTest.java was modified

--- src/test/java/ru/netology/qahwweb/appordertests/AppOrderTest.java
+++ src/test/java/ru/netology/qahwweb/appordertests/AppOrderTest.java
@@ -12,5 +12,11 @@ public class AppOrderTest {
     @Test
     void shouldCreateRequestPositivePath() {
         open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id=name] input").setValue("Анна Шевченко");
+        form.$("[data-test-id=phone] input").setValue("+79960000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[class='button__content']").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
